b4c34a86a664abf9c67519cfd7606f0b9ac0bf8f
tvmalaysia11 <tvmalaysia11@gmail.com>
2025-04-26 03:09:48 +0700

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -1,9 +1,10 @@
 console.clear()
 
-// Ambil ID dari URL format /contentDetails/abc123
-let id = location.pathname.split('/')[2];
-console.log("Extracted ID:", id); // <-- tambahkan ini
+// Ambil ID dari URL, misalnya ?abc123
+let id = location.search.split('?')[1]
+console.log("ID:", id)
 
+// Cek cookie dan tampilkan jumlah item di badge
 if (document.cookie.indexOf(',counter=') >= 0) {
     let counter = document.cookie.split(',')[1].split('=')[1]
     document.getElementById("badge").innerHTML = counter
@@ -51,13 +52,13 @@ function dynamicContentDetails(ob) {
     productPreviewDiv.appendChild(h3ProductPreviewDiv)
 
     for (let i = 0; i < ob.photos.length; i++) {
-        let previewImg = document.createElement('img')
-        previewImg.id = 'previewImg'
-        previewImg.src = ob.photos[i]
-        previewImg.onclick = function () {
-            imgTag.src = this.src
+        let imgTagProductPreviewDiv = document.createElement('img')
+        imgTagProductPreviewDiv.id = 'previewImg'
+        imgTagProductPreviewDiv.src = ob.photos[i]
+        imgTagProductPreviewDiv.onclick = function () {
+            document.getElementById("imgDetails").src = this.src
         }
-        productPreviewDiv.appendChild(previewImg)
+        productPreviewDiv.appendChild(imgTagProductPreviewDiv)
     }
 
     let buttonDiv = document.createElement('div')
@@ -65,6 +66,7 @@ function dynamicContentDetails(ob) {
 
     let buttonTag = document.createElement('button')
     buttonTag.appendChild(document.createTextNode('Add to Cart'))
+
     buttonTag.onclick = function () {
         let order = id + " "
         let counter = 1
@@ -89,21 +91,23 @@ function dynamicContentDetails(ob) {
     detailsDiv.appendChild(para)
     productDetailsDiv.appendChild(productPreviewDiv)
     productDetailsDiv.appendChild(buttonDiv)
+
+    return mainContainer
 }
 
-// Fetch product data
+// Ambil data dari product.json dan cari yang cocok dengan ID
 let httpRequest = new XMLHttpRequest()
 httpRequest.onreadystatechange = function () {
     if (this.readyState === 4 && this.status == 200) {
         console.log('connected!!')
-        let data = JSON.parse(this.responseText)
-        let contentDetails = data.find(item => item.id === id)
+        let contentList = JSON.parse(this.responseText)
 
-        if (contentDetails) {
-            dynamicContentDetails(contentDetails)
+        let product = contentList.find(item => item.id === id)
+
+        if (product) {
+            dynamicContentDetails(product)
         } else {
-            console.log("Product not found with id:", id)
-            document.getElementById("containerProduct").innerHTML = "<p>Product not found.</p>"
+            console.error("Product not found with id:", id)
         }
     } else if (this.readyState === 4) {
         console.log('not connected!')
