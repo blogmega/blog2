c4cf4313fee0f228404bf401a3ed278d36a76056
tvmalaysia11 <tvmalaysia11@gmail.com>
2025-04-26 00:24:44 +0700

    Update content.js

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ function dynamicClothingSection(ob) {
 
   let boxLink = document.createElement("a");
   // boxLink.href = '#'
-   boxLink.href = "/contentdetails.html/" + ob.id;
+   boxLink.href = "/contentdetails/" + ob.id;
   // console.log('link=>' + boxLink);
 
   let imgTag = document.createElement("img");
